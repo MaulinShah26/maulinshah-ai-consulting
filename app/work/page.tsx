744b86aff3da6590d0ad10ad62859026aee21e40
Maulin S <22667469+MaulinShah26@users.noreply.github.com>
2026-08-17 12:00:26 +0530

    Keep work page focused on case studies

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,7 +1,6 @@
 import { Nav } from "@/components/Nav";
 import { FeaturedWork, featuredCorporateHrefs } from "@/components/FeaturedWork";
 import { Work } from "@/components/Work";
-import { TestimonialProof } from "@/components/TestimonialProof";
 import { Footer } from "@/components/Footer";
 
 export default function WorkPage() {
@@ -10,7 +9,6 @@ export default function WorkPage() {
       <Nav />
       <main className="pt-6">
         <FeaturedWork />
-        <TestimonialProof person="kuntal" label="From CricHeroes leadership" />
         <Work sectionLabel="More work" excludeCorporateHrefs={featuredCorporateHrefs} />
       </main>
       <Footer />
